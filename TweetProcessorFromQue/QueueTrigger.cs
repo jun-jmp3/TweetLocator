660c4598743ee339aad54ae8b09d33197501df94
JFurukawa <jun.jmp3@gmail.com>
2018-07-07 18:29:33 +0900

    テーブル構造の修正。

--- TweetProcessorFromQue/QueueTrigger.cs
+++ TweetProcessorFromQue/QueueTrigger.cs
@@ -14,10 +14,11 @@ namespace TweetProcessorFromQue
     public class TweetLocationTable {
         public string PartitionKey { get; set; }
         public string RowKey { get; set; }
+        public long TweetID { get; set; }
         public DateTime TweetTime { get; set; }
         public string Text { get; set; }
-//        public double Latitude { get; set; }
-//        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
     }
 
     public static class QueueTrigger
@@ -26,9 +27,7 @@ namespace TweetProcessorFromQue
         [FunctionName("QueueTrigger")]
         public static void Run(
             [QueueTrigger("wug-tweets-que", Connection = "AzureWebJobsStorage")] string myQueueItem, 
-            [Table("TweetLocation", Connection = "AzureWebJobsStorage")] 
-            ICollector<TweetLocationTable> locationTable,
-//            [Table("OutTable", Connection = "AzureWebJobsStorage")] out string outputTable,
+            [Table("TweetLocation", Connection = "AzureWebJobsStorage")] ICollector<TweetLocationTable> locationTable,
             TraceWriter log, 
             ExecutionContext context)
         {
@@ -43,6 +42,7 @@ namespace TweetProcessorFromQue
                     .AddEnvironmentVariables()
                     .Build();
 
+                // 環境変数からキーを取得する
                 string consumerKey = config["ConsumerKey"];
                 string consumerSecret = config["ConsumerSecret"];
                 string accessToken = config["AccessToken"];
@@ -51,60 +51,85 @@ namespace TweetProcessorFromQue
                 if (string.IsNullOrEmpty(consumerKey) || string.IsNullOrEmpty(consumerSecret) || string.IsNullOrEmpty(accessToken) || string.IsNullOrEmpty(accessTokenSecret)) {
                     log.Error("can't read configuration about token.");
                     return;
-//                    lo = null;
                 }
 
+                // アクセストークン
                 var tokens = Tokens.Create(consumerKey, consumerSecret, accessToken, accessTokenSecret);
                 // log.Info($"twitter token created: {tokens.ToString()}");
 
+                // ツイートを取得
                 var task = tokens.Statuses.ShowAsync(id => myQueueItem);
                 var status = task.Result;
 
-                // RowKeyの重複回避のためランダムな文字列を生成する
-                Random random = new Random();
-                string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-                string randomStr = new string(Enumerable.Repeat(chars, 32)
-                    .Select(s => s[random.Next(s.Length)]).ToArray());
+                // ツイートを記録する
+                InsertRecord(locationTable, status, log);
 
-                // SampleTableへEntity(レコード)登録
-                locationTable.Add(
-                    new TweetLocationTable()
-                    {
-                        PartitionKey = "PremiumUser",
-                        RowKey = randomStr,
-                        TweetTime = DateTime.Now,
-                        Text = status.Text
-                    }
-                );
-
-                var place = status.Place;
-                if (status.Coordinates != null || place != null)
-                {
-                    log.Info($"{status.User.ScreenName} {status.Text}");
-                }
-                if (status.Coordinates != null) {
-                    log.Info($"Status Coordinates: {status.Coordinates.Longitude},{status.Coordinates.Latitude}");
-                }
-
-                if (place != null) {
-                    log.Info($"Place: {place.FullName},{place.Id}");
-
-                    if (place.Centroid != null) {                        
-                        foreach (var oid in place.Centroid )
-                            log.Info($"Centoroid {oid}");
-                    }
-
-                    if (place.Geometry != null)
-                    {
-                        log.Info($"Long: {place.Geometry.Longitude}, Lat:{place.Geometry.Latitude}");
-                    }
-                }
             }
             catch (Exception ex)
             {
                 log.Error($"{ex.Message}");
                 log.Error($"{ex.StackTrace}");
             }
+        }
+
+        /// <summary>
+        /// Inserts the record.
+        /// </summary>
+        private static void InsertRecord(ICollector<TweetLocationTable> locationTable,
+                                         StatusResponse status,
+                                         TraceWriter log) {
+            
+            double latitude = 0.0;
+            double longitude = 0.0;
+            var place = status.Place;
+            if (status.Coordinates != null || place != null)
+            {
+                log.Info($"{status.User.ScreenName} {status.Text}");
+            }
+
+
+
+            if (status.Coordinates != null)
+            {
+                log.Info($"Status Coordinates: {status.Coordinates.Longitude},{status.Coordinates.Latitude}");
+            }
+
+            if (place != null)
+            {
+                log.Info($"Place: {place.FullName},{place.Id}");
+
+                if (place.Centroid != null)
+                {
+                    foreach (var oid in place.Centroid)
+                        log.Info($"Centoroid {oid}");
+                }
+
+                if (place.Geometry != null)
+                {
+                    log.Info($"Long: {place.Geometry.Longitude}, Lat:{place.Geometry.Latitude}");
+                }
+            }
+
+            // RowKeyの重複回避のためランダムな文字列を生成する
+            Random random = new Random();
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            string randomStr = new string(Enumerable.Repeat(chars, 32)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+
+            // SampleTableへEntity(レコード)登録
+            locationTable.Add(new TweetLocationTable()
+            {
+                PartitionKey = "k1",
+                RowKey = randomStr,
+                TweetID = status.Id,
+                TweetTime = status.CreatedAt.UtcDateTime,
+                Text = status.Text,
+                Latitude = latitude,
+                Longitude = longitude
+            }
+                             );
+
+
         }
 
     }
